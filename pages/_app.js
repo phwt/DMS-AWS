935ab3b8d9b978b0f5132cfede9a21d7946b6df1
phwt.smwt <phwt.smwt@gmail.com>
2021-04-05 18:33:23 +0700

    Config bootstrap and react-bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "../styles/globals.css";
+import "bootstrap/scss/bootstrap.scss";
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
