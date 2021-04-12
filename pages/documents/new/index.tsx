cbcb98f39f52d13dc287a19e50b1fae0ca618974
phwt.smwt <phwt.smwt@gmail.com>
2021-04-12 21:15:45 +0700

    Convert to react-boostrap form

--- pages/documents/new/index.tsx
+++ pages/documents/new/index.tsx
@@ -1,37 +1,46 @@
+import { Col, Form, Row } from "react-bootstrap";
+
 const DocumentNew = () => {
-  return <>
-    <h2 className="pb-5">Document Creation Form</h2>
-    <form action="" method="post" encType="multipart/form-data">
+  return (
+    <>
+      <h2 className="pb-5">Document Creation Form</h2>
+      <Form as={Row} encType="multipart/form-data">
+        <Col md={6}>
+          <Form.Label>Document Name</Form.Label>
+          <Form.Control />
+        </Col>
 
+        <Col md={6}>
+          <Form.Label>Document Type</Form.Label>
+          <Form.Control as="select" custom>
+            <option value="MANUAL">Manual</option>
+            <option value="PROCEDURE">Procedure</option>
+            <option value="WORK_INSTRUCTION">Work Instruction</option>
+            <option value="FORM">Form</option>
+          </Form.Control>
+        </Col>
 
-      {/* form.name.errors  */}
-      <label htmlFor="form.name.id_for_label"> form.name.label </label>
-      {/* {{ form.name }} */}
-
-      {/* {{ form.source.errors }} */}
-      <label htmlFor="{{ form.source.id_for_label }}"> form.source.label </label>
-      {/* form.source }} */}
-
-      {/* {{ form.detail.errors }} */}
-      <label htmlFor="{{ form.detail.id_for_label }}"> form.detail.label </label>
-      {/* {{ form.detail }} */}
-
-      {/* {{ form.file_location.errors }} */}
-      <label htmlFor="{{ form.file_location.id_for_label }}"> form.file_location.label </label>
-      <div className="custom-file">
-        <input
-          type="file"
-          className="custom-file-input"
-          name="{{ form.file_location.name }}"
-          id="{{ form.file_location.id_for_label }}"
-          value="form.file_location.value"
-        />
-          <label className="custom-file-label" htmlFor="customFile">Choose file</label>
-          {/* {#            {{ form.file_location }}#} */}
+        <Col md={12} className="mt-2">
+          <Form.Label>Document File</Form.Label>
+          <div className="custom-file">
+            <input type="file" className="custom-file-input" />
+            <label className="custom-file-label" htmlFor="customFile">
+              Choose file
+            </label>
           </div>
-        <input type="submit" className="btn btn-info mt-3" value="Submit"/>
-      </form>
-  </>;
+        </Col>
+
+        <Col md={12} className="mt-2">
+          <Form.Label>Work Detail</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Col>
+
+        <Col md={12}>
+          <input type="submit" className="btn btn-info mt-3" value="Submit" />
+        </Col>
+      </Form>
+    </>
+  );
 };
 
 export default DocumentNew;
