78d850d36b1f04bc7783a4fb50f716c6a592854b
phwt.smwt <phwt.smwt@gmail.com>
2021-04-12 21:15:31 +0700

    Fix logo path

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
           <nav className="navbar navbar-expand-lg navbar-dark">
             <a className="navbar-brand" href="/">
               <img
-                src="dms_logo.png"
+                src="/dms_logo.png"
                 width="30"
                 height="30"
                 className="d-inline-block align-top"
