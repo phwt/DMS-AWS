4238b3662df6a2df84b8cfd9fef5713fb52b74c5
phwt.smwt <phwt.smwt@gmail.com>
2021-04-13 12:57:18 +0700

    Dashboard API

---
+++ pages/api/dashboard.ts
@@ -0,0 +1,117 @@
+import { PrismaClient } from "@prisma/client";
+import { apiWrapper } from "@modules/Utils";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default apiWrapper(async (req: NextApiRequest, res: NextApiResponse) => {
+  let result;
+
+  const totalDocuments = async () => {
+    const {
+      count: { id },
+    } = await prisma.document.aggregate({
+      count: {
+        id: true,
+      },
+    });
+
+    return id;
+  };
+  const aggregateDocumentState = async (state) => {
+    const {
+      count: { id },
+    } = await prisma.document.aggregate({
+      count: {
+        id: true,
+      },
+      where: {
+        state,
+      },
+    });
+    return id;
+  };
+  const latestDocuments = async () => {
+    return await prisma.document.findMany({
+      orderBy: {
+        id: "desc",
+      },
+      take: 10,
+    });
+  };
+  const latestDocumentsByState = async (state) => {
+    return await prisma.document.findMany({
+      where: {
+        state,
+      },
+      orderBy: {
+        id: "desc",
+      },
+      take: 10,
+    });
+  };
+
+  const totalWorks = async () => {
+    const {
+      count: { id },
+    } = await prisma.work.aggregate({
+      count: {
+        id: true,
+      },
+    });
+
+    return id;
+  };
+  const aggregateWorkType = async (type) => {
+    const {
+      count: { id },
+    } = await prisma.work.aggregate({
+      count: {
+        id: true,
+      },
+      where: {
+        type,
+      },
+    });
+    return id;
+  };
+  const latestWorks = async () => {
+    return await prisma.work.findMany({
+      orderBy: {
+        id: "desc",
+      },
+      take: 10,
+    });
+  };
+
+  switch (req.method) {
+    case "GET":
+      result = {
+        summary: {
+          document: {
+            total: await totalDocuments(),
+            inProgress: await aggregateDocumentState("IN_PROGRESS"),
+            released: await aggregateDocumentState("RELEASED"),
+            obsoleted: await aggregateDocumentState("OBSOLETE"),
+          },
+          work: {
+            total: await totalWorks(),
+            create: await aggregateWorkType("CREATE"),
+            edit: await aggregateWorkType("EDIT"),
+            cancel: await aggregateWorkType("CANCEL"),
+          },
+        },
+        latestDocuments: {
+          all: await latestDocuments(),
+          inProgress: await latestDocumentsByState("IN_PROGRESS"),
+          released: await latestDocumentsByState("RELEASED"),
+        },
+        latestWorks: {
+          all: await latestWorks(),
+        },
+      };
+
+      res.status(200).json(result);
+      break;
+  }
+});
