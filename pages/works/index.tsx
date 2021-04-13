b0583a6840e3b2b45252cbbfea19c78dd5348b4d
phwt.smwt <phwt.smwt@gmail.com>
2021-04-13 12:19:58 +0700

    Show work list

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,72 +1,79 @@
-const WorkList = () => {
+import axios from "axios";
+import { useCallback } from "react";
+import { Form } from "react-bootstrap";
+
+export const getServerSideProps = async (ctx) => {
+  const { data } = await axios.get(`${process.env.API_PATH}works/`);
+
+  return {
+    props: {
+      works: data,
+    },
+  };
+};
+
+const WorkList = ({ works }) => {
+  const workTypeBadge = useCallback((type) => {
+    switch (type) {
+      case "CREATE":
+        return <span className="badge badge-pill badge-success">Create</span>;
+      case "EDIT":
+        return <span className="badge badge-pill badge-warning">Edit</span>;
+      case "CANCEL":
+        return <span className="badge badge-pill badge-danger">Cancel</span>;
+    }
+  }, []);
+
   return (
     <>
-    <h2 className="pb-5">View All Works</h2>
-    <form method="GET" action="">
+      <h2 className="pb-5">View All Works</h2>
+      <form method="GET" action="">
         <div className="table-responsive table-mh">
-            <table className="table table-borderless text-light">
-                <thead className="bg-0">
-                <tr>
-                    <th scope="col">Related Document</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">State</th>
-                    <th scope="col">Create Date</th>
-                    <th scope="col">Complete Date</th>
-                    <th scope="col">Delegate User</th>
-                    <th scope="col"></th>
-                </tr>
-                </thead>
-                <tr>
-                    <td>filter_forms.document }}</td>
-                    <td>filter_forms.type }}</td>
-                    <td>filter_forms.state }}</td>
-                    <td></td>
-                    <td></td>
-                    <td>filter_forms.employee }}</td>
-                    <td>
-                        <button type="submit" className="btn btn-info btn-block">
-                            <i className="fa fa-search" aria-hidden="true"></i>
-                        </button>
-                    </td>
-                </tr>
-                {/* {% for work in works %} */}
-                    <tr>
-                        <td>
-                            <a href="{% url 'work_detail' id=work.id %}">work }}</a>
-                        </td>
-                        {/* {% if work.get_type_display == 'Create' %} */}
-                            <td><span className="badge badge-pill badge-success">work.get_type_display }}</span></td>
-                        {/* {% elif work.get_type_display == 'Edit' %} */}
-                            <td><span className="badge badge-pill badge-warning">work.get_type_display }}</span></td>
-                        {/* {% elif work.get_type_display == 'Cancel' %} */}
-                            <td><span className="badge badge-pill badge-danger">work.get_type_display }}</span></td>
-                        {/* {% endif %} */}
-                        <td>work.get_state_display }}</td>
-                        <td>work.create_date }}</td>
-                        {/* {% if work.complete_date == null %} */}
-                            <td>-</td>
-                        {/* {% else %} */}
-                            <td>work.complete_date }}</td>
-                        {/* {% endif %} */}
-                        {/* {% if work.latest_delegate == null %} */}
-                            <td>-</td>
-                        {/* {% else %} */}
-                            <td>
-                                <a href="{% url 'profile-other' id=work.latest_delegate.user_id %}">work.latest_delegate }}</a>
-                            </td>
-                        {/* {% endif %} */}
-                        <td>
-                            <a href="{% url 'work_detail' id=work.id %}">
-                                <button type="button" className="btn btn-block">
-                                    <i className="fa fa-chevron-right text-info" aria-hidden="true"></i>
-                                </button>
-                            </a>
-                        </td>
-                    </tr>
-                {/* {% endfor %} */}
-            </table>
+          <table className="table table-borderless text-light">
+            <thead className="bg-0">
+              <tr>
+                <th scope="col">Related Document</th>
+                <th scope="col">Type</th>
+                <th scope="col">State</th>
+                <th scope="col">Create Date</th>
+                <th scope="col">Complete Date</th>
+                <th scope="col" />
+              </tr>
+            </thead>
+            <tr>
+              <td />
+              <td>
+                {/* TODO: Live filter by type and state */}
+                <Form.Control as="select" size="sm" />{" "}
+              </td>
+              <td>
+                <Form.Control as="select" size="sm" />
+              </td>
+              <td />
+              <td />
+              <td />
+            </tr>
+
+            {works.map((work) => (
+              <tr>
+                <td>{work.document.name}</td>
+                <td>{workTypeBadge(work.type)}</td>
+                <td>{work.state[0] + work.state.toLowerCase().slice(1)}</td>
+                <td>{work.create_date}</td>
+                <td>{work.complete_date ?? "-"}</td>
+                <td>
+                  <button type="button" className="btn btn-block">
+                    <i
+                      className="fa fa-chevron-right text-info"
+                      aria-hidden="true"
+                    />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
-    </form>
+      </form>
     </>
   );
 };
