f5afc67cda85fc44d458177b34215f3328946c05
phwt.smwt <phwt.smwt@gmail.com>
2021-04-14 17:25:46 +0700

    Link with RDS

--- prisma/migrations/20210414102459_init/migration.sql
+++ prisma/migrations/20210414102459_init/migration.sql
@@ -5,7 +5,7 @@ CREATE TYPE "DocumentType" AS ENUM ('MANUAL', 'PROCEDURE', 'WORK_INSTRUCTION', '
 CREATE TYPE "DocumentState" AS ENUM ('IN_PROGRESS', 'RELEASED', 'OBSOLETE', 'RECALLED');
 
 -- CreateEnum
-CREATE TYPE "WorkType" AS ENUM ('CREATE', 'EDIT', 'CANCLE');
+CREATE TYPE "WorkType" AS ENUM ('CREATE', 'EDIT', 'CANCEL');
 
 -- CreateEnum
 CREATE TYPE "WorkState" AS ENUM ('NEW', 'REVIEW', 'COMPLETED');
@@ -16,6 +16,7 @@ CREATE TABLE "Document" (
     "name" TEXT NOT NULL,
     "type" "DocumentType" NOT NULL,
     "state" "DocumentState" NOT NULL DEFAULT E'IN_PROGRESS',
+    "fileLocation" TEXT NOT NULL,
 
     PRIMARY KEY ("id")
 );
@@ -28,10 +29,13 @@ CREATE TABLE "Work" (
     "detail" TEXT NOT NULL,
     "create_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "complete_date" TIMESTAMP(3),
-    "documentId" INTEGER NOT NULL,
+    "documentId" INTEGER,
 
     PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Work_documentId_unique" ON "Work"("documentId");
+
 -- AddForeignKey
-ALTER TABLE "Work" ADD FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Work" ADD FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE SET NULL ON UPDATE CASCADE;
