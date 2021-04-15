5eb1cec48791876a5156a5a2311a35252751a0cb
phwt.smwt <phwt.smwt@gmail.com>
2021-04-15 14:28:34 +0700

    Add action box for new state

--- pages/works/[workId]/index.tsx
+++ pages/works/[workId]/index.tsx
@@ -1,5 +1,8 @@
 import { useRouter } from "next/router";
 import axios from "axios";
+import { WorkTypeBadge } from "../index";
+import { documentTypeText } from "../../documents";
+import { useMemo } from "react";
 
 export const getServerSideProps = async (context) => {
   const { data } = await axios.get(
@@ -15,18 +18,27 @@ const Work = ({ work }) => {
   const router = useRouter();
   const { workId } = router.query;
 
+  const workTitle = useMemo(() => {
+    switch (work.type) {
+      case "CREATE":
+        return `Request to create ${work.document.name}`;
+      case "EDIT":
+        return `Request to edit ${work.document.name}`;
+      case "CANCEL":
+        return `Request to cancel ${work.document.name}`;
+    }
+  }, []);
+
   return (
     <div>
       <div className="row">
         <div className="col-3">
           <h2 className="pb-5">Work Detail</h2>
         </div>
-        <div className="col-9 text-right">
-          <div className="mt-2">{work.state}</div>
-        </div>
+        <div className="col-9 text-right" />
       </div>
 
-      <h3>work</h3>
+      <h3>{workTitle}</h3>
       <p className="text-muted">
         <small>{work.create_date}</small>
       </p>
@@ -41,11 +53,13 @@ const Work = ({ work }) => {
                 </tr>
                 <tr>
                   <th scope="row">Type</th>
-                  <td>{work.type}</td>
+                  <td>
+                    <WorkTypeBadge type={work.type} />
+                  </td>
                 </tr>
                 <tr>
                   <th scope="row">State</th>
-                  <td>{work.state}</td>
+                  <td>{work.state[0] + work.state.slice(1).toLowerCase()}</td>
                 </tr>
                 <tr>
                   <th scope="row">Complete Date</th>
@@ -65,7 +79,7 @@ const Work = ({ work }) => {
               <div className="card-body">
                 <div className="card-text">
                   <b>Name</b>: {work.document.name} <br />
-                  <b>Type</b>: {work.document.type} <br />
+                  <b>Type</b>: {documentTypeText(work.document.type)} <br />
                   <a className="btn btn-info mt-3">View Document</a>
                 </div>
               </div>
@@ -74,17 +88,34 @@ const Work = ({ work }) => {
         </div>
       </div>
 
-      <div className="col-3 p-0">
-        <div className="card bg-dark mt-3">
-          <div className="card-header">
-            <h5>Review Action</h5>
-          </div>
-          <div className="card-body">
-            <button className="btn btn-block btn-success">Approve</button>
-            <button className="btn btn-block btn-danger">Reject</button>
+      {work.state === "NEW" && (
+        <div className="col-3 p-0">
+          <div className="card bg-dark mt-3">
+            <div className="card-header">
+              <h5 className="m-0">Action</h5>
+            </div>
+            <div className="card-body">
+              <button className="btn btn-block btn-info">
+                Submit For Review
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+
+      {work.state === "REVIEW" && (
+        <div className="col-3 p-0">
+          <div className="card bg-dark mt-3">
+            <div className="card-header">
+              <h5 className="m-0">Review Actions</h5>
+            </div>
+            <div className="card-body">
+              <button className="btn btn-block btn-success">Approve</button>
+              <button className="btn btn-block btn-danger">Reject</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
