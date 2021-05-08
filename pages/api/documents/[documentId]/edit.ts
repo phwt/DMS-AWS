60fd8451ed03ce4ff016e8c97a8fcfab4862f7e6
phwt.smwt <phwt.smwt@gmail.com>
2021-05-08 20:11:48 +0700

    Update edit.ts

--- pages/api/documents/[documentId]/edit.ts
+++ pages/api/documents/[documentId]/edit.ts
@@ -2,15 +2,47 @@ import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import AWS from "aws-sdk";
+
+// Multer 'files' object
+declare module "next" {
+  interface NextApiRequest {
+    files: {
+      fieldname: string;
+      originalname: string;
+      encoding: string;
+      mimetype: string;
+      size: number;
+      bucket: string;
+      key: string;
+      acl: string;
+      contentType: string;
+      // contentDisposition: null,
+      storageClass: string;
+      // serverSideEncryption: null,
+      // metadata: null,
+      location: string;
+      etag: string;
+      versionId: undefined;
+    }[];
+  }
+}
 
 const prisma = new PrismaClient();
 const apiRoute = nextConnect();
 
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
 // Upload Middleware
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: "./public/uploads", // TODO: Send to S3
-    filename: (req, file, cb) => cb(null, file.originalname),
+  storage: multerS3({
+    s3,
+    bucket: process.env.S3_BUCKET,
+    key: (req, file, cb) => cb(null, file.originalname),
   }),
 });
 const uploadMiddleware = upload.array("file");
@@ -35,7 +67,7 @@ apiRoute.post(async (req: NextApiRequest, res: NextApiResponse) => {
           name: req.body.name,
           type: editDocument.type,
           state: "IN_PROGRESS",
-          fileLocation: req.files[0].path,
+          fileLocation: req.files[0].location,
         },
       },
     },
