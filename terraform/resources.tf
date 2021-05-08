d84c7f212eb4fbc6d85833fea1b4b228981e80f4
phwt.smwt <phwt.smwt@gmail.com>
2021-05-08 20:56:29 +0700

    Base networking resources

---
+++ terraform/resources.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+# DATA #
+
+data "aws_availability_zones" "available" {}
+
+# NETWORKING #
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.network_address_space
+  tags       = merge(local.mandatory_tags, { Name = "${var.project_name}-VPC" })
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = merge(local.mandatory_tags, { Name = "${var.project_name}-IGW" })
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = 3
+  cidr_block              = cidrsubnet(var.network_address_space, 8, count.index)
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(local.mandatory_tags, { Name = "${var.project_name} Public Subnet - ${data.aws_availability_zones.available.names[count.index]}" })
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = 3
+  cidr_block        = cidrsubnet(var.network_address_space, 8, (count.index + 3))
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(local.mandatory_tags, { Name = "${var.project_name} Private Subnet - ${data.aws_availability_zones.available.names[count.index]}" })
+}
+
+# ROUTING #
+
+resource "aws_route_table" "public_rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(local.mandatory_tags, { Name = "${var.project_name}-public-rtb" })
+}
+
+resource "aws_route_table" "private_rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(local.mandatory_tags, { Name = "${var.project_name}-private-rtb" })
+}
+
+resource "aws_route_table_association" "public-rta-subnet" {
+  count          = 3
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rtb.id
+}
+
+resource "aws_route_table_association" "private-rta-subnet" {
+  count          = 3
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_rtb.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+  type = string
+  default = "DMS"
+}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+  default = "ap-southeast-1"
+}
+variable "network_address_space" {
+  type    = string
+  default = "10.25.0.0/16"
+}
+
+locals {
+  mandatory_tags = {
+    itcourse = "cb21"
+    itgroup  = "cb21group25"
+  }
+}
