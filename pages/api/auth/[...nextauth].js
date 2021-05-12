bfe32991e5908031c220f8f5278ab6d6b16649fa
phwt.smwt <phwt.smwt@gmail.com>
2021-05-12 20:39:38 +0700

    Pass user group to session context

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import jwtDecode from "jwt-decode";
 
 export default NextAuth({
   providers: [
@@ -9,4 +10,22 @@ export default NextAuth({
       domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
     }),
   ],
+  callbacks: {
+    async jwt(token, user, account, profile, isNewUser) {
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    async session(session, token) {
+      try {
+        return {
+          ...session,
+          groups: jwtDecode(token.accessToken)["cognito:groups"],
+        };
+      } catch {
+        return session;
+      }
+    },
+  },
 });
