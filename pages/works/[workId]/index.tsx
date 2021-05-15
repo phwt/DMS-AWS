104d16402011c3e35558d5028c58609c9d5e3627
phwt.smwt <phwt.smwt@gmail.com>
2021-05-15 23:35:33 +0700

    Fix import

--- pages/works/[workId]/index.tsx
+++ pages/works/[workId]/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import axios from "axios";
-import { WorkTypeBadge } from "../index";
+import WorkTypeBadge from "@components/WorkTypeBadge";
 import { documentTypeText } from "../../documents";
 import { useCallback, useMemo, useState } from "react";
 import { restrictPage } from "@modules/Auth";
