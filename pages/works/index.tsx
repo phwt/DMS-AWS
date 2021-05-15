63523cb490fadaa5c4ea0d320988bb70fc5355ca
maizerocom <maizerocom@gmail.com>
2021-05-15 22:10:27 +0700

    Sort the latest information first in work list and document list and Fixed warning in console alert :rotating_light:

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -9,7 +9,7 @@ export const getServerSideProps = async (context) => {
   await restrictPage(context);
   const serverUser = await getSession(context);
   const { data } = await axios.get(`${process.env.API_PATH}works/`);
-
+  data.sort((a, b) => (a.id < b.id) ? 1 : -1)
   return {
     props: {
       works: data,
@@ -53,70 +53,72 @@ const WorkList = ({ works, serverUser }) => {
                 <th scope="col" />
               </tr>
             </thead>
-            <tr>
-              <td />
-              <td>
-                {/* TODO: Live filter by type and state */}
-                <Form.Control
-                  as="select"
-                  size="sm"
-                  onChange={(e) => setTypeSelect(e.target.value.toUpperCase())}
-                >
-                  {workType.map((type) => (
-                    <option>{type}</option>
-                  ))}
-                </Form.Control>
-              </td>
-              <td>
-                <Form.Control
-                  as="select"
-                  size="sm"
-                  onChange={(e) => setStateSelect(e.target.value.toUpperCase())}
-                >
-                  {workState.map((type) => (
-                    <option>{type}</option>
-                  ))}
-                </Form.Control>
-              </td>
-              <td />
-              <td />
-              <td />
-            </tr>
+            <tbody>
+              <tr>
+                <td />
+                <td>
+                  {/* TODO: Live filter by type and state */}
+                  <Form.Control
+                    as="select"
+                    size="sm"
+                    onChange={(e) => setTypeSelect(e.target.value.toUpperCase())}
+                  >
+                    {workType.map((type) => (
+                      <option key={type}>{type}</option>
+                    ))}
+                  </Form.Control>
+                </td>
+                <td>
+                  <Form.Control
+                    as="select"
+                    size="sm"
+                    onChange={(e) => setStateSelect(e.target.value.toUpperCase())}
+                  >
+                    {workState.map((type) => (
+                      <option key={type}>{type}</option>
+                    ))}
+                  </Form.Control>
+                </td>
+                <td />
+                <td />
+                <td />
+              </tr>
 
-            {works
-              .filter((w) => w.type === typeSelect || typeSelect === "-")
-              .filter((w) => w.state === stateSelect || stateSelect === "-")
-              .map((work) => {
-                if (!(userGroup === "Employee" && work.state === "REVIEW")) {
-                  return (
-                    <tr key={work.id}>
-                      <td>{work.document.name}</td>
-                      <td>
-                        <WorkTypeBadge type={work.type} />
-                      </td>
-                      <td>
-                        {work.state[0] + work.state.toLowerCase().slice(1)}
-                      </td>
-                      <td>{work.create_date}</td>
-                      <td>{work.complete_date ?? "-"}</td>
-                      <td>
-                        <button
-                          type="button"
-                          className="btn btn-block"
-                          onClick={async () => {
-                            await router.push(`/works/${work.id}`);
-                          }}
-                        >
-                          <i
-                            className="fa fa-chevron-right text-info"
-                            aria-hidden="true"
-                          />
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                }
-              })}
+              {works
+                .filter((w) => w.type === typeSelect || typeSelect === "-")
+                .filter((w) => w.state === stateSelect || stateSelect === "-")
+                .map((work) => {
+                  if (!(userGroup === "Employee" && work.state === "REVIEW")) {
+                    return (
+                      <tr key={work.id}>
+                        <td>{work.document.name}</td>
+                        <td>
+                          <WorkTypeBadge type={work.type} />
+                        </td>
+                        <td>
+                          {work.state[0] + work.state.toLowerCase().slice(1)}
+                        </td>
+                        <td>{work.create_date}</td>
+                        <td>{work.complete_date ?? "-"}</td>
+                        <td>
+                          <button
+                            type="button"
+                            className="btn btn-block"
+                            onClick={async () => {
+                              await router.push(`/works/${work.id}`);
+                            }}
+                          >
+                            <i
+                              className="fa fa-chevron-right text-info"
+                              aria-hidden="true"
+                            />
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  }
+                })}
+            </tbody>
           </table>
         </div>
       </form>
